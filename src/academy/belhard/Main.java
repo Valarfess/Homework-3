ca6ead026ea31745103d5c79ecda562a22e84d72
valarfess <valarfess@gmail.com>
2021-02-14 23:23:48 +0300

    Домашка 3

--- src/academy/belhard/Main.java
+++ src/academy/belhard/Main.java
@@ -3,20 +3,27 @@ package academy.belhard;
 public class Main {
 
     public static void main(String[] args) {
-        int month = 10;
+//        int month = 10;
+//
+//        if (month == 12 || month == 1 || month == 2) {
+//            System.out.print("Зима");
+//        }
+//            else if (month == 3 || month == 4 || month == 5){
+//                System.out.print("Весна");
+//        }
+//        else if (month == 6 || month == 7 || month == 8) {
+//            System.out.print("Лето");
+//        }
+//        else if (month == 9 || month == 10 || month == 11) {
+//            System.out.print("Осень");
+//        }
 
-        if (month == 12 || month == 1 || month == 2) {
-            System.out.print("Зима");
+        int [] array = {0,1,2,3,4,5,6,7,8,9};
+
+        for (int i=0; i>array.length; i--)
+        {
+            System.out.println(array[i]);
         }
-            else if (month == 3 || month == 4 || month == 5){
-                System.out.print("Весна");
-        }
-        else if (month == 6 || month == 7 || month == 8) {
-            System.out.print("Лето");
-        }
-        else if (month == 9 || month == 10 || month == 11) {
-            System.out.print("Осень");
-        }
-	// write your code here
+        // write your code here
     }
 }
