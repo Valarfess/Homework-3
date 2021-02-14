104eaec0151a2a4241261ebf9a213e43600260f9
valarfess <valarfess@gmail.com>
2021-02-14 23:38:41 +0300

    Домашка 3

--- src/academy/belhard/Main.java
+++ src/academy/belhard/Main.java
@@ -18,12 +18,37 @@ public class Main {
 //            System.out.print("Осень");
 //        }
 
+
+//        int month = 8;
+//        switch (month) {
+//            case 12:
+//            case 1:
+//            case 2:
+//                System.out.println("Зима");
+//                break;
+//            case 3:
+//            case 4:
+//            case 5:
+//                System.out.println("Весна");
+//                break;
+//            case 6:
+//            case 7:
+//            case 8:
+//                System.out.println("Лето");
+//                break;
+//            case 9:
+//            case 10:
+//            case 11:
+//                System.out.println("осень");
+//                break;
+//        }
+
+
         int [] array = {0,1,2,3,4,5,6,7,8,9};
 
-        for (int i=0; i>array.length; i--)
-        {
-            System.out.println(array[i]);
-        }
+        for (int i=9; i< array.length; i--)
+        { System.out.println(array[i]); }
+
         // write your code here
     }
 }
